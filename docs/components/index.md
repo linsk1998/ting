035444bfbb5b290fea3f3e9ad49ceb402f9495a6
linsk <linsk@live.com>
2026-08-12 22:28:46 +0800

    navbar-sticky-top

--- docs/components/index.md
+++ docs/components/index.md
@@ -48,7 +48,7 @@ breadcrumb在一个带有层次的导航结构中标明当前页面的位置。
 | 类 | 描述 |
 |---|---|
 | .navbar | 导航栏基础容器，提供基本的定位和布局 |
-| .navbar-flush | 清除导航栏的边框和内边距 |
+| .navbar-sticky-top | 导航栏固定在顶部 |
 | .navbar-brand | 导航栏品牌标识区域，通常放置Logo或网站名称 |
 | .navbar-nav | 导航栏内的导航列表容器 |
 | .navbar-collapse | 响应式折叠区域，在小屏幕时可收起/展开 |
--- scripts/gen-site-header.js
+++ scripts/gen-site-header.js
@@ -16,31 +16,30 @@ function navbar(currentSection) {
 							<li${classAttr}><a href="${prefix}${escapeHtml(section)}/index.html">${escapeHtml(SECTION_MAP[section])}</a></li>`;
 	}
 
-	return `<div class="navbar"><div class="navbar-brand"></div></div>
-			<div class="navbar navbar-fixed-top">
-				<div class="container">
-					<div class="navbar-header">
-						<button type="button" class="navbar-toggle fa icon-lg">&#xf0c9;</button>
-						<a href="${prefix}index.html" class="navbar-brand"><img height="37" src="${prefix}images/logo.png"/><span class="valign-sibling">Ting Web UI</span></a>
-					</div>
-					<div class="navbar-responsive-dropdown">
-						<ul class="navbar-nav">${navbarHtml}
-						</ul>
-						<ul class="navbar-nav navbar-right">
-							<li><a class="px-2" href="https://github.com/linsk1998/ting/" target="_blank"><i class="icon-fw icon-lg fa">&#xf09b;</i></a></li>
-						</ul>
-						<script>initDarkLi()</script>
-					</div>
+	return `<div class="navbar navbar-sticky-top">
+			<div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle fa icon-lg">&#xf0c9;</button>
+					<a href="${prefix}index.html" class="navbar-brand"><img height="37" src="${prefix}images/logo.png"/><span class="valign-sibling">Ting Web UI</span></a>
 				</div>
-			</div>`;
+				<div class="navbar-responsive-dropdown">
+					<ul class="navbar-nav">${navbarHtml}
+					</ul>
+					<ul class="navbar-nav navbar-right">
+						<li><a class="px-2" href="https://github.com/linsk1998/ting/" target="_blank"><i class="icon-fw icon-lg fa">&#xf09b;</i></a></li>
+					</ul>
+					<script>initDarkLi()</script>
+				</div>
+			</div>
+		</div>`;
 }
 
 /**
  * 生成页面头部（导航条 + jumbotron，不含 DOCTYPE/<head>/<body>）
  */
 function header(currentSection) {
-	return `<header>
-			${navbar(currentSection)}
+	return `${navbar(currentSection)}
+		<header>
 			<div class="jumbotron">
 				<div class="container">
 					<h1 class="h1">Ting Web UI</h1>
